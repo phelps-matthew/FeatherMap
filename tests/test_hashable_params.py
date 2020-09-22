aac05967685d06814721d53cb1f440fe48345d3a
phelps-matthew <mgp333@gmail.com>
2020-09-22 11:59:32 -0700

    add batch norm filter test

--- tests/test_hashable_params.py
+++ tests/test_hashable_params.py
@@ -1,4 +1,9 @@
+"""
+Implementation testing for hashable parameters function.
+"""
+# Many imports derived from PyTorch source
 import torch
+from torch.nn.modules import Module
 import torch.nn as nn
 from torch.nn import Parameter
 from src.resnet import ResNet, ResidualBlock
@@ -18,22 +23,119 @@ from typing import (
     Dict,
 )
 
-# parameters(named_parameters(_named_members(named_modules(named_modules))))
 
 # Device configuration
 my_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = ResNet(ResidualBlock, [2, 2, 2]).to(my_device)
 
+# print(type(model.parameters()))
+# print(model.parameters().__next__())
+# print(next(model.named_parameters()))
+
 test_params = list(model.parameters())
 test_named_params = dict(model.named_parameters())
 
-print(*test_named_params.keys(), sep="\n")
+# print(*test_named_params.keys(), sep="\n")
+# print(*model.named_modules(), sep='\n')
+l = nn.Linear(2, 2)
+net1 = nn.Sequential(nn.Linear(2, 2), nn.Linear(2, 2))
+net = nn.Sequential(l, l, net1)
+# print(*dict(net.parameters()),sep='\n')
+
+for name, module in model.named_modules():
+    # print(name, type(module))
+    continue
+
+#print(l._parameters.items())
+
+
+def my_named_members(self, get_members_fn, prefix="", recurse=True):
+    r"""Helper method for yielding various names + members of modules."""
+    memo = set()
+    # iterator
+    modules = self.named_modules(prefix=prefix) if recurse else [(prefix, self)]
+    for module_prefix, module in modules:
+        if isinstance(module, nn.BatchNorm2d):
+            print("Caught one\n {} {}".format(module_prefix, type(module)))
+            continue
+        # members from _parameters.items() are odict_items (possibly empty)
+        members = get_members_fn(module)
+        for k, v in members:
+            if v is None or v in memo:
+                continue
+            memo.add(v)
+            name = module_prefix + ("." if module_prefix else "") + k
+            yield name, v
+
+
+def my_named_parameters(
+    self, prefix: str = "", recurse: bool = True
+) -> Iterator[Tuple[str, Tensor]]:
+    r"""Returns an iterator over module parameters, yielding both the
+    name of the parameter as well as the parameter itself.
+    Args:
+        prefix (str): prefix to prepend to all parameter names.
+    """
+    gen = my_named_members(
+        self,
+        lambda module: module._parameters.items(),
+        prefix=prefix,
+        recurse=recurse,
+    )
+    for elem in gen:
+        yield elem
+
+print(*dict(my_named_parameters(model)).keys(), sep='\n')
+print('-'*20)
+print(*dict(model.named_parameters()).keys(), sep='\n')
 
 
 # ---------------------------------------------------------------------------- #
 # PyTorch methods                                                              #
 # ---------------------------------------------------------------------------- #
+
+# parameters(named_parameters(_named_members(named_modules(named_modules))))
+# net._parameters only exists for explicitly defined layers (i.e. empty for nn.Sequential or nn.ResNet)
+# for a given module, seeks __getattr__;
+
+
+def parameters(self, recurse: bool = True) -> Iterator[Parameter]:
+    for name, param in self.named_parameters(recurse=recurse):
+        yield param
+
+
+def named_parameters(
+    self, prefix: str = "", recurse: bool = True
+) -> Iterator[Tuple[str, Tensor]]:
+    r"""Returns an iterator over module parameters, yielding both the
+    name of the parameter as well as the parameter itself.
+    Args:
+        prefix (str): prefix to prepend to all parameter names.
+    """
+    gen = self._named_members(
+        lambda module: module._parameters.items(),
+        prefix=prefix,
+        recurse=recurse,
+    )
+    for elem in gen:
+        yield elem
+
+
+def _named_members(self, get_members_fn, prefix="", recurse=True):
+    r"""Helper method for yielding various names + members of modules."""
+    memo = set()
+    # iterator
+    modules = self.named_modules(prefix=prefix) if recurse else [(prefix, self)]
+    for module_prefix, module in modules:
+        # members from _parameters.items() are odict_items (possibly empty)
+        members = get_members_fn(module)
+        for k, v in members:
+            if v is None or v in memo:
+                continue
+            memo.add(v)
+            name = module_prefix + ("." if module_prefix else "") + k
+            yield name, v
 
 
 def named_modules(self, memo: Optional[Set["Module"]] = None, prefix: str = ""):
@@ -55,6 +157,7 @@ def named_modules(self, memo: Optional[Set["Module"]] = None, prefix: str = ""):
         ))
         1 -> ('0', Linear(in_features=2, out_features=2, bias=True))
     """
+    # non-duplication should not be issue; weights are same in ex.
 
     if memo is None:
         memo = set()
@@ -67,62 +170,3 @@ def named_modules(self, memo: Optional[Set["Module"]] = None, prefix: str = ""):
             submodule_prefix = prefix + ("." if prefix else "") + name
             for m in module.named_modules(memo, submodule_prefix):
                 yield m
-
-
-def named_parameters(
-    self, prefix: str = "", recurse: bool = True
-) -> Iterator[Tuple[str, Tensor]]:
-    r"""Returns an iterator over module parameters, yielding both the
-    name of the parameter as well as the parameter itself.
-    Args:
-        prefix (str): prefix to prepend to all parameter names.
-        recurse (bool): if True, then yields parameters of this module
-            and all submodules. Otherwise, yields only parameters that
-            are direct members of this module.
-    Yields:
-        (string, Parameter): Tuple containing the name and parameter
-    Example::
-        >>> for name, param in self.named_parameters():
-        >>>    if name in ['bias']:
-        >>>        print(param.size())
-    """
-    gen = self._named_members(
-        lambda module: module._parameters.items(),
-        prefix=prefix,
-        recurse=recurse,
-    )
-    for elem in gen:
-        yield elem
-
-
-def parameters(self, recurse: bool = True) -> Iterator[Parameter]:
-    r"""Returns an iterator over module parameters.
-    This is typically passed to an optimizer.
-    Args:
-        recurse (bool): if True, then yields parameters of this module
-            and all submodules. Otherwise, yields only parameters that
-            are direct members of this module.
-    Yields:
-        Parameter: module parameter
-    Example::
-        >>> for param in model.parameters():
-        >>>     print(type(param), param.size())
-        <class 'torch.Tensor'> (20L,)
-        <class 'torch.Tensor'> (20L, 1L, 5L, 5L)
-    """
-    for name, param in self.named_parameters(recurse=recurse):
-        yield param
-
-
-def _named_members(self, get_members_fn, prefix="", recurse=True):
-    r"""Helper method for yielding various names + members of modules."""
-    memo = set()
-    modules = self.named_modules(prefix=prefix) if recurse else [(prefix, self)]
-    for module_prefix, module in modules:
-        members = get_members_fn(module)
-        for k, v in members:
-            if v is None or v in memo:
-                continue
-            memo.add(v)
-            name = module_prefix + ("." if module_prefix else "") + k
-            yield name, v
