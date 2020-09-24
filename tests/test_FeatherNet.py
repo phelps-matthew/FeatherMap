e8959fe659eb003acdec8f7d7a5deb534712ad21
phelps-matthew <mgp333@gmail.com>
2020-09-24 13:54:00 -0700

    test WandBtoV, passed

--- tests/test_FeatherNet.py
+++ tests/test_FeatherNet.py
@@ -30,29 +30,26 @@ class FeatherNet(nn.Module):
         self.compress = compress
         self.size_n = ceil(sqrt(self._num_WandB(exclude=exclude)))
         self.size_m = ceil((self.compress * self.size_n) / 2)
-        self.V1 = Parameter(Tensor(self.size_n, self.size_m))
-        self.V2 = Parameter(Tensor(self.size_n, self.size_m))
+        self.V1 = Parameter(torch.randn(self.size_n, self.size_m))
+        self.V2 = Parameter(torch.randn(self.size_n, self.size_m))
+        self.V = torch.matmul(self.V1, self.V2.transpose(0, 1))
 
-    def _num_WorB(self, exclude: tuple = (), kind: str = "weight"):
-        """Return total number of weights or biases"""
-        return sum(
-            p.numel() for n, p in self.get_WorB(exclude=exclude, kind=kind)
-        )
+    def WandBtoV(self, exclude: tuple = ()):
+        i, j = 0, 0
+        V = self.V.view(-1, 1)
+        for name, v in self.get_WandB(exclude=exclude):
+            v = v.view(-1, 1)
+            for j in range(len(v)):
+                v[j] = V[i]
+                i += 1
 
     def _num_WandB(self, exclude: tuple = ()):
         """Return total number of weights and biases"""
-        return sum(
-            v.numel() for name, v in self.get_WandB(exclude=exclude)
-        )
-
-    def get_WorB(self, exclude: tuple = (), kind: str = "weight"):
-        for name, module in self.get_modules(exclude=exclude, kind=kind):
-            name = name + "." + kind
-            yield name, getattr(module, kind)
+        return sum(v.numel() for name, v in self.get_WandB(exclude=exclude))
 
     def get_WandB(self, exclude: tuple = ()):
         for name, module, kind in self.get_WandB_modules(exclude=exclude):
-            yield name + '.' + kind, getattr(module, kind)
+            yield name + "." + kind, getattr(module, kind)
 
     def get_WandB_modules(self, exclude: tuple = ()):
         """Helper function to return weight and bias modules in order"""
@@ -61,11 +58,22 @@ class FeatherNet(nn.Module):
                 if isinstance(module, exclude):
                     continue
                 if getattr(module, "weight") is not None:
-                    yield name, module, 'weight'
+                    yield name, module, "weight"
                 if getattr(module, "bias") is not None:
-                    yield name, module, 'bias'
+                    yield name, module, "bias"
             except nn.modules.module.ModuleAttributeError:
                 pass
+
+    def _num_WorB(self, exclude: tuple = (), kind: str = "weight"):
+        """Return total number of weights or biases"""
+        return sum(
+            p.numel() for n, p in self.get_WorB(exclude=exclude, kind=kind)
+        )
+
+    def get_WorB(self, exclude: tuple = (), kind: str = "weight"):
+        for name, module in self.get_modules(exclude=exclude, kind=kind):
+            name = name + "." + kind
+            yield name, getattr(module, kind)
 
     def get_modules(self, exclude: tuple = (), kind: str = "weight"):
         """Helper function to return weight or bias modules"""
@@ -146,23 +154,6 @@ class FeatherNet(nn.Module):
                 name = module_prefix + ("." if module_prefix else "") + k
                 yield name, v
 
-    def hparams_to_W(self) -> Tensor:
-        """Collect set of hashable params into n x n tensor"""
-        pass
-
-    def mhash(self, W: Tensor, compress: float) -> Tensor:
-        """Create hash tensor (matrix product multi-hash)"""
-        # dim_W = torch.size(W)[0]
-        # target_size = dim_W*compress
-        # M = Parameter(torch.Tensor(target_size, target_size))
-        # MT = Parameter(torch.transpose(M))
-        pass
-
-    def reassign_hparams(self, W, M):
-        """Deparameterize hash parameters, points towards hash tensor"""
-        pass
-
-
 def main():
     # parameters(named_parameters(_named_members(named_modules(named_modules))))
 
@@ -171,31 +162,46 @@ def main():
     my_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = ResNet(ResidualBlock, [2, 2, 2]).to(my_device)
     f_model = FeatherNet(model)
-    # [print(name) for name, v in f_model.named_parameters()]
+    lmodel = nn.Linear(2, 4)
+    flmodel = FeatherNet(lmodel, compress=0.5)
+    flmodel.unregister_params()
+    flmodel.unregister_params(kind="bias")
+    print(flmodel._num_WandB(), flmodel.size_n, flmodel.size_m)
+    a = lmodel.weight
+    print(a)
+    print(flmodel.V)
+    flmodel.WandBtoV()
+    f_model.unregister_params()
+    f_model.unregister_params(kind="bias")
+    f_model.WandBtoV()
+    print("V1: {}".format(f_model.V1))
+    print("V2: {}".format(f_model.V2))
+    print("V: {}".format(f_model.V))
+    [print(name, v) for name, v in f_model.named_parameters()]
     # print(f_model.parameter_pop())
     # [print(name) for name, v in f_model.named_parameters()]
     # print(*list(filter(lambda x: not(isinstance(x, nn.BatchNorm2d)), f_model.modules())),sep='\n')
-    [print(name) for name, v in f_model.named_parameters()]
-    print("-" * 20)
+    # [print(name) for name, v in f_model.named_parameters()]
+    # print("-" * 20)
     # f_model.del_params()
     # [print(name + '.weight') for name, m, in f_model.get_weights(exclude=(nn.BatchNorm2d))]
     # [print(name + '.bias') for name, m, in f_model.get_bias()]
-    for name, weight in f_model.get_modules(exclude=(nn.BatchNorm2d)):
-        print(name)
-    f_model.unregister_params()
+    # for name, weight in f_model.get_modules(exclude=(nn.BatchNorm2d)):
+    #     print(name)
+    # f_model.unregister_params()
     # print("-" * 20)
-    [print(name) for name, v in f_model.named_parameters()]
-    print("-" * 20)
-    [print(name) for name, v in f_model.get_WandB(exclude=(nn.BatchNorm2d))]
-    #print(model.conv.weight.numel())
-    #print(model.conv.weight.size())
-    #print(f_model._num_WorB())
-    #print(f_model._num_WorB(kind="bias"))
-    a = f_model._num_WorB() + f_model._num_WorB(kind="bias")
-    print(a)
-    print(ceil(sqrt(a)))
-    print(f_model.size_n)
-    #print(443 ** 2)
+    # [print(name) for name, v in f_model.named_parameters()]
+    # print("-" * 20)
+    # [print(name) for name, v in f_model.get_WandB(exclude=(nn.BatchNorm2d))]
+    # print(model.conv.weight.numel())
+    # print(model.conv.weight.size())
+    # print(f_model._num_WorB())
+    # print(f_model._num_WorB(kind="bias"))
+    # a = f_model._num_WorB() + f_model._num_WorB(kind="bias")
+    # print(a)
+    # print(ceil(sqrt(a)))
+    # print(f_model.size_n)
+    # print(443 ** 2)
     exit()
 
     # print(*dict(model.named_parameters()).keys(), sep="\n")
