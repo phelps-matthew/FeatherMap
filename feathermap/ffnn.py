6bb29fa640ab890cd6fa2785408b02c3ec3aec31
phelps-matthew <mgp333@gmail.com>
2020-09-26 22:21:25 -0700

    rm data

--- feathermap/ffnn.py
+++ feathermap/ffnn.py
@@ -4,7 +4,7 @@ from torch import nn
 
 class FFNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
+        super().__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, num_classes)
