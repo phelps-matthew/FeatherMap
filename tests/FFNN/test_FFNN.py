d881d831d34e33302f450c36142b9a6f1a7fece0
phelps-matthew <mgp333@gmail.com>
2020-09-27 09:57:54 -0700

    update todo
    Former-commit-id: d8c447040e28ced8204761268d1e709180b0947e

--- tests/FFNN/test_FFNN.py
+++ tests/FFNN/test_FFNN.py
@@ -12,7 +12,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 input_size = 784
 hidden_size = 500
 num_classes = 10
-num_epochs = 1
+num_epochs = 5
 batch_size = 100
 learning_rate = 0.001
 
