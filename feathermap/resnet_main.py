40ea731ce79e9d46f0ae86a66019306afe6358b8
phelps-matthew <mgp333@gmail.com>
2020-09-27 08:16:17 -0700

    add retnset argparse

--- feathermap/resnet_main.py
+++ feathermap/resnet_main.py
@@ -12,117 +12,153 @@ import torchvision
 import torchvision.transforms as transforms
 from feathermap.resnet import ResidualBlock, ResNet
 from feathermap.feathernet import FeatherNet
-
-# Device configuration
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Hyper-parameters
-num_epochs = 80
-batch_size = 100
-learning_rate = 0.001
-
-# Image preprocessing modules
-transform = transforms.Compose(
-    [
-        transforms.Pad(4),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(32),
-        transforms.ToTensor(),
-    ]
-)
-
-# CIFAR-10 dataset
-train_dataset = torchvision.datasets.CIFAR10(
-    root="./data/", train=True, transform=transform, download=True
-)
-
-test_dataset = torchvision.datasets.CIFAR10(
-    root="./data/", train=False, transform=transforms.ToTensor()
-)
-
-# Data loader
-train_loader = torch.utils.data.DataLoader(
-    dataset=train_dataset, batch_size=batch_size, shuffle=True
-)
-
-test_loader = torch.utils.data.DataLoader(
-    dataset=test_dataset, batch_size=batch_size, shuffle=False
-)
+import argparse
 
 
-# 3x3 convolution
-def conv3x3(in_channels, out_channels, stride=1):
-    return nn.Conv2d(
-        in_channels,
-        out_channels,
-        kernel_size=3,
-        stride=stride,
-        padding=1,
-        bias=False,
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="ResNet34 on CIFAR10 with Structured Multi-Hashing compression",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("--epochs", type=int, default=80, help="Number of epochs")
+    parser.add_argument("--batch-size", type=int, default=100, help="Mini-batch size")
+    parser.add_argument("--lr", type=float, default=0.001, help="Learning rate at t=0")
+    parser.add_argument(
+        "--compress",
+        type=float,
+        default=0.5,
+        help="Compression rate. Set to zero for base model",
+    )
+    parser.add_argument(
+        "--save-model",
+        action="store_true",
+        default=False,
+        help="Save model in local directory",
+    )
+    args = parser.parse_args()
+
+    print(args)
+    return args
+
+
+def load_data(batch_size):
+    # Image preprocessing modules
+    transform = transforms.Compose(
+        [
+            transforms.Pad(4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32),
+            transforms.ToTensor(),
+        ]
     )
 
+    # CIFAR-10 dataset
+    train_dataset = torchvision.datasets.CIFAR10(
+        root="./data/", train=True, transform=transform, download=True
+    )
 
-base_model = ResNet(ResidualBlock, [2, 2, 2]).to(device)
-model = FeatherNet(base_model, exclude=(nn.BatchNorm2d), compress=0.25)
+    test_dataset = torchvision.datasets.CIFAR10(
+        root="./data/", train=False, transform=transforms.ToTensor()
+    )
 
-# Loss and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # Data loader
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset, batch_size=batch_size, shuffle=True
+    )
+
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_dataset, batch_size=batch_size, shuffle=False
+    )
+    return train_loader, test_loader
 
 
-# For updating learning rate
-def update_lr(optimizer, lr):
-    for param_group in optimizer.param_groups:
-        param_group["lr"] = lr
+def train(model, train_loader, epochs, lr, device):
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
+    # For updating learning rate
+    def update_lr(optimizer, lr):
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = lr
 
-# Train the model
-total_step = len(train_loader)
-curr_lr = learning_rate
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        images = images.to(device)
-        labels = labels.to(device)
+    # Train the model
+    total_step = len(train_loader)
+    curr_lr = lr
+    for epoch in range(epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.to(device)
+            labels = labels.to(device)
 
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        if (i + 1) % 100 == 0:
-            print(
-                "Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}".format(
-                    epoch + 1, num_epochs, i + 1, total_step, loss.item()
+            if (i + 1) % 100 == 0:
+                print(
+                    "Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}".format(
+                        epoch + 1, epochs, i + 1, total_step, loss.item()
+                    )
                 )
-            )
 
-    # Decay learning rate
-    if (epoch + 1) % 20 == 0:
-        curr_lr /= 3
-        update_lr(optimizer, curr_lr)
+        # Decay learning rate
+        if (epoch + 1) % 20 == 0:
+            curr_lr /= 3
+            update_lr(optimizer, curr_lr)
 
-# Test the model
-model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
 
-    print(
-        "Accuracy of the model on the test images: {} %".format(
-            100 * correct / total
-        )
-    )
+def evaluate(model, test_loader, device):
+    # Test the model
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-# Save the model checkpoint
-torch.save(model.state_dict(), "resnet.ckpt")
+        accuracy = 100 * correct / total
+
+        print("Accuracy of the model on the test images: {} %".format(accuracy))
+        return accuracy
+
+
+def main():
+    args = parse_arguments()
+
+    # Device configuration
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Select model
+    base_model = ResNet(ResidualBlock, [2, 2, 2])
+    if args.compress:
+        model = FeatherNet(base_model, exclude=(nn.BatchNorm2d), compress=args.compress)
+    else:
+        model = base_model.to(device)
+
+    # Load data
+    train_loader, test_loader = load_data(args.batch_size)
+
+    # Train, evaluate
+    train(model, train_loader, args.epochs, args.lr, device)
+    evaluate(model, test_loader, device)
+
+    # Save the model checkpoint
+    if args.save_model:
+        torch.save(model.state_dict(), "resnet.ckpt")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
