82d6e7983e4be6f9bc7279d54c27b5467289eeee
phelps-matthew <mgp333@gmail.com>
2020-09-27 09:43:43 -0700

    mv arg parse to class
    Former-commit-id: 4cff0634204236df22fb0b49c7bcdb09a8c73727

--- feathermap/models/ffnn.py
+++ feathermap/models/ffnn.py
@@ -1,5 +1,39 @@
 """Fully connected feed forward neural network with one hidden layer"""
 from torch import nn
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="FFNN on MNIST with Structured Multi-Hashing compression",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("--hidden-size", type=int, default=500, help="Hidden size")
+    parser.add_argument("--epochs", type=int, default=5, help="Number of epochs")
+    parser.add_argument("--batch-size", type=int, default=100, help="Mini-batch size")
+    parser.add_argument("--lr", type=float, default=0.001, help="Learning rate at t=0")
+    parser.add_argument(
+        "--num-workers",
+        type=int,
+        default=1,
+        help="Number of dataloader processing threads. Try adjusting for faster training",
+    )
+    parser.add_argument(
+        "--compress",
+        type=float,
+        default=0.5,
+        help="Compression rate. Set to zero for base model",
+    )
+    parser.add_argument(
+        "--save-model",
+        action="store_true",
+        default=False,
+        help="Save model in local directory",
+    )
+    args = parser.parse_args()
+
+    print(args)
+    return args
 
 
 class FFNN(nn.Module):
