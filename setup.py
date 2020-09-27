5ce90d786faab4a030a343ec90f560eeb1d34fff
phelps-matthew <mgp333@gmail.com>
2020-09-27 13:18:51 -0700

    update dockerfile for entrypoint

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="feathermap",
     version="1.0",
-    packages=find_packages(),
+    #packages=find_packages(),
     install_requires=[
         "torch",
         "torchvision",
-        "numpy",
-        "pandas",
-        "matplotlib",
-        "streamlit"
+        #"numpy",
+        #"pandas",
+        #"matplotlib",
+        #"streamlit"
         # "pathlib",
         # "sklearn",
         # "ipython",
