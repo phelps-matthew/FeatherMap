40d745fc132eac5d79230fc726cc7b6aee801ad1
phelps-matthew <mgp333@gmail.com>
2020-09-28 15:21:48 -0700

    update gpu cuda enabling

--- feathermap/utils/timer.py
+++ feathermap/utils/timer.py
@@ -1,4 +1,5 @@
 from timeit import default_timer as timer
+import torch
 
 
 def timed(method):
@@ -10,3 +11,18 @@ def timed(method):
         return result
 
     return time_me
+
+
+def print_gpu_status():
+    """Print GPU torch cuda status"""
+    try:
+        cuda_status = [
+            "torch.cuda.device(0): {}".format(torch.cuda.device(0)),
+            "torch.cuda.device_count(): {}".format(torch.cuda.device_count()),
+            "torch.cuda.get_device_name(0): {}".format(torch.cuda.get_device_name(0)),
+            "torch.cuda_is_available: {}".format(torch.cuda.is_available()),
+            "torch.cuda.current_device: {}".format(torch.cuda.current_device()),
+        ]
+        print(*cuda_status, sep="\n")
+    except:
+        print("Some torch.cuda functionality unavailable")
