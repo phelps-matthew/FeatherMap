6d3c10431b4e2133eefb3947348dad4de0c19b05
phelps-matthew <mgp333@gmail.com>
2020-10-08 21:56:19 -0700

    to main train fix test_loader bug

--- feathermap/train/main.py
+++ feathermap/train/main.py
@@ -160,7 +160,7 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(valid_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            progress_bar(batch_idx, len(test_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
 
