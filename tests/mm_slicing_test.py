5340b48f680b875c26e00dd0c6ccf00b56d5594a
phelps-matthew <mgp333@gmail.com>
2020-10-17 18:54:11 -0700

    to mm_slciing_test create new get_block_rows fn; test coverage

--- tests/mm_slicing_test.py
+++ tests/mm_slicing_test.py
@@ -1,18 +1,33 @@
 import torch
+from typing import List
 
 
-def get_block_rows(i, j, numels, n):
-    j_start = j == 0
-    rows = []
-    for _ in range(numels+72):
-        if j > n:
-            if j_start:
-                rows.append(i)
-            i = i + 1
-            j = 0
-            j_start = True
-        j = j + 1
-    return rows
+def get_block_rows(i1: int, j1: int, i2: int, j2: int, n: int) -> List[int]:
+    """Return range of full (complete) rows"""
+    row = []
+    # Handle all cases for j1=0
+    if j1 == 0:
+        # All row(s) complete
+        if j2 == n - 1:
+            row.extend(set([i1, i2]))
+            return sorted(row)
+        # First row complete, last row incomplete
+        elif i2 > i1:
+            row.extend(set([i1, i2 - 1]))
+            return sorted(row)
+        # First row incomplete (from right), no additional rows
+        else:
+            return row
+    # First row incomplete (from left), last row complete
+    if j2 == n - 1 and i2 > i1:
+        row.extend(set([i1 + 1, i2]))
+        return sorted(row)
+    # First row incomplete, last row incomplete; has at least one full row
+    if i2 - i1 > 1:
+        row.extend(set([i1 + 1, i2 - 1]))
+        return sorted(row)
+    # First row incomplete, second row incomplete
+    return row
 
 
 def get_row_set(V1, V2, i1, j1, i2, j2, numels, n):
@@ -67,12 +82,32 @@ n, m = 3342, 1
 V1 = torch.Tensor([list(range(n * q, n * (q + 1))) for q in range(m)]).reshape(n, m)
 V2 = torch.Tensor([list(range(m * r, m * (r + 1))) for r in range(n)]).reshape(m, n)
 V = torch.Tensor([list(range(n * q, n * (q + 1))) for q in range(n)])
-#print(V1)
-#print(V2)
-#print(V)
-numels = 589824
-(i1, j1, i2, j2) = (652, 1816, 829, 105)
-#(i1, j1, i2, j2) = (476,184,652,1815)
-print(get_block_rows(i1, j1, numels, n))
-#print(get_row_set_V(V, i1, j1, i2, j2, numels, n))
-#print(get_row_set(V1, V2, i1, j1, i2, j2, numels, n))
+# numels = 589824
+n = 3342
+
+# ---------------------- #
+# Coverage Tests ------- #
+# ---------------------- #
+
+# All one row complete
+print(get_block_rows(i1=42, j1=0, i2=42, j2=n - 1, n=n) == [42])
+# All two rows complete
+print(get_block_rows(i1=652, j1=0, i2=653, j2=n - 1, n=n) == [652, 653])
+# All 10 rows complete
+print(get_block_rows(i1=652, j1=0, i2=662, j2=n - 1, n=n) == [652, 662])
+# First row complete, second incomplete
+print(get_block_rows(i1=0, j1=0, i2=1, j2=n - 100, n=n) == [0])
+# First row compete, last row incomplete
+print(get_block_rows(i1=652, j1=0, i2=829, j2=105, n=n) == [652, 828])
+# First row incomplete (from right), no additional rows
+print(get_block_rows(i1=1, j1=0, i2=1, j2=105, n=n) == [])
+# First row incomplete (from left), no additional rows
+print(get_block_rows(i1=123, j1=123, i2=123, j2=n - 1, n=n) == [])
+# First row incomplete (from left), second row incomplete
+print(get_block_rows(i1=222, j1=1816, i2=223, j2=2018, n=n) == [])
+# First row incomplete (from left), last row incomplete
+print(get_block_rows(i1=652, j1=1816, i2=829, j2=105, n=n) == [653, 828])
+# First row incomplete (from left), second row complete
+print(get_block_rows(i1=3000, j1=1816, i2=3001, j2=n-1, n=n) == [3001])
+# First row incomplete (from left), last row complete
+print(get_block_rows(i1=3000, j1=1816, i2=3091, j2=n-1, n=n) == [3001, 3091])
