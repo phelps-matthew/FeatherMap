91603289296750a03f52c948284fc3a3b839bd84
phelps-matthew <mgp333@gmail.com>
2020-12-12 17:24:49 -0800

    add __init__s
    Former-commit-id: 8b3046329ffc97027a9ed5268c62e539722f547c

--- tests/unit/test_matrix_slicing.py
+++ tests/unit/test_matrix_slicing.py
@@ -1,34 +1,7 @@
 import torch
-from typing import List
-
-
-def get_block_rows(i1: int, j1: int, i2: int, j2: int, n: int) -> List[int]:
-    """Return range of full (complete) rows from an (n x n) matrix, starting from row, col
-    [i1, j1] and ending at [i2, j2]. Returns range [row_begin, row_end + 1)"""
-    row = []
-    # Handle all cases for j1=0
-    if j1 == 0:
-        # All row(s) complete
-        if j2 == n - 1:
-            row.extend([i1, i2 + 1])
-            return row
-        # First row complete, last row incomplete
-        elif i2 > i1:
-            row.extend([i1, i2])
-            return row
-        # First row incomplete (from right), no additional rows
-        else:
-            return row
-    # First row incomplete (from left), last row complete
-    if j2 == n - 1 and i2 > i1:
-        row.extend([i1 + 1, i2 + 1])
-        return row
-    # First row incomplete, last row incomplete; has at least one full row
-    if i2 - i1 > 1:
-        row.extend([i1 + 1, i2])
-        return row
-    # First row incomplete, second row incomplete
-    return row
+from feathermap.utils import get_block_rows
+from feathermap import feathernet
+import unittest
 
 
 def get_row_set(V1, V2, i1, j1, i2, j2, numels, n):
